899d6547131d49266da7f6a9035cecc8670870aa
Marco Arnone <marco.arnone@spindox.it>
2022-03-03 10:41:50 +0100

    adding validation to every route

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,13 @@ export class TasksService {
     }
 
     getTaskById(id: string) {
-        return this.tasks.find((task) => task.id === id);
+        const found = this.tasks.find((task) => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -54,7 +60,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter((task) => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
